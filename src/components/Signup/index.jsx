aff5e0e484ccde237f0a05e0de1ad0454b257415
Kumar Shubham <kumarshubham.ksh@gmail.com>
2024-07-23 21:48:15 +0530

    Create index.jsx

---
+++ src/components/Signup/index.jsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./styles.module.css";
+import Swal from "sweetalert2";
+
+const Signup = () => {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "https://petify-backend.onrender.com/api/users";
+      const { data: res } = await axios.post(url, data);
+      navigate("/login");
+      console.log(res.message);
+      Swal.fire({
+        title: "Registered Successfully!",
+        text: "You are now registered with Petify!",
+        icon: "success",
+      });
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <div className={styles.signup_container}>
+      <div className={styles.signup_form_container}>
+        <div className={styles.left}>
+          <h1>Welcome Back!</h1>
+          <Link to="/login">
+            <button type="button" className={styles.white_btn}>
+              Sign In
+            </button>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <form className={styles.form_container} onSubmit={handleSubmit}>
+            <h1>Sign Up</h1>
+            <input
+              type="text"
+              placeholder="Enter your first name"
+              name="firstName"
+              onChange={handleChange}
+              value={data.firstName}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Enter your last name"
+              name="lastName"
+              onChange={handleChange}
+              value={data.lastName}
+              required
+              className={styles.input}
+            />
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className={styles.input}
+            />
+            {error && <div className={styles.error_msg}>{error}</div>}
+            <button type="submit" className={styles.green_btn}>
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
